5ddd6d21e3b558bf6d247239f3e922d1ef1f0ca8
TABISH KHAN <tabish003@hotmail.com>
2016-03-23 22:56:15 -0700

    add new js file for recent articles

---
+++ app/assets/javascripts/blogjs.js
@@ -0,0 +1,24 @@
+// FACE BOOK RECENT ARTICLES FEATURE
+
+// $(document).on('ready page:load', function(){
+
+//   $(window).scroll(function(){
+
+//     var userScroll = $(window).scrollTop();
+//     console.log(userScroll);
+    
+//     if (userScroll > 0 && userScroll < 364) {
+//       $('.blog-sidebar').addClass('change-display');
+//       $('.change-display').css('top', 364-userScroll);
+//     }
+//     else if (userScroll > 0 && userScroll > 364 ) {
+//       $('.blog-sidebar').removeAttr('style');
+//     }
+//     else {
+//       $('.blog-sidebar').removeClass('change-display');
+//       $('.blog-sidebar').removeAttr('style');
+//     }
+
+//   });
+
+// });
