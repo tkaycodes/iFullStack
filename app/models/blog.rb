17f4f11b3cf0f4e1a5b9b3ec874ff899fe06155c
TABISH KHAN <tabish003@hotmail.com>
2016-04-10 14:45:42 -0700

    added to_param method to blog model - this is to show the blog name in url

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,10 @@
 class Blog < ActiveRecord::Base
   has_many :tagged_blogposts;
   has_many :tags, through: :tagged_blogposts;
+
+  # override rails method for converting blog object to url param
+  def to_param
+    "#{id} #{title}".parameterize
+  end
+
 end
